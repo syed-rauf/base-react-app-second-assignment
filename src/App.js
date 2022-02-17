643d24ea923ddc995514e6582a7920ebbdca8e68
syed-rauf <masterrufi.it@gmail.com>
2022-02-17 20:44:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Second Assignment
+          Second Assignment completed by Syed Rauf
         </p>
         <a
           className="App-link"
