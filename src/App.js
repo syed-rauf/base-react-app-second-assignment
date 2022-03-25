7879834650c45e646803d2192102635ae048d2ff
syed-rauf <masterrufi.it@gmail.com>
2022-03-26 01:12:47 +0500

    link remove from app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Second Assignment completed by Syed Rauf
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Second Assignment completed by Syed Rauf</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
